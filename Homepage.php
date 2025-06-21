54f1111379195139f482e955e7424b2dc1bc6ec8
WeiJie <thangweijie@gmail.com>
2025-06-21 14:16:15 +0800

    Added a temporary homepage

---
+++ Homepage.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php if($_SESSION["ROLE"] == "admin"): ?>
+        <h1>You are an admin</h1>
+    <?php endif; ?>
+    <?php if($_SESSION["ROLE"] == "lecturer"): ?>
+        <a href="feedback.html">Feedback Form</a> <br><br>
+        <a href="booking.html">Booking Application</a> <br> <br>
+        <a href="availableRoom.php">Browse Available Rooms</a>
+    <?php endif; ?>
+    <?php if($_SESSION["ROLE"] == "space_manager"): ?>
+        <h1>You are a space manager</h1>
+    <?php endif; ?>
+</body>
+</html>
