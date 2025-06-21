aa363d2938e759c0f4ba94f793140fa0bff052fc
WeiJie <thangweijie@gmail.com>
2025-06-21 15:15:16 +0800

    Added logout to homepage for all roles

--- Homepage.php
+++ Homepage.php
@@ -1,5 +1,10 @@
 <?php
     session_start();
+
+    if(isset($_GET['LOGOUT'])) {
+        session_destroy();
+        header("Location: login.php");
+    }
 ?>
 
 <!DOCTYPE html>
@@ -12,14 +17,17 @@
 <body>
     <?php if($_SESSION["ROLE"] == "admin"): ?>
         <h1>You are an admin</h1>
+        <a href="Homepage.php?LOGOUT=1">Logout</a>
     <?php endif; ?>
     <?php if($_SESSION["ROLE"] == "lecturer"): ?>
         <a href="feedback.html">Feedback Form</a> <br><br>
         <a href="booking.html">Booking Application</a> <br> <br>
-        <a href="availableRoom.php">Browse Available Rooms</a>
+        <a href="availableRoom.php">Browse Available Rooms</a> <br>
+        <a href="Homepage.php?LOGOUT=1">Logout</a>  
     <?php endif; ?>
     <?php if($_SESSION["ROLE"] == "space_manager"): ?>
         <h1>You are a space manager</h1>
+        <a href="Homepage.php?LOGOUT=1">Logout</a>
     <?php endif; ?>
 </body>
 </html>
