e42e489029c2e55a7c0d4c2b84347b9d55df902b
WeiJie <thangweijie@gmail.com>
2025-06-22 10:43:35 +0800

    Added all links

--- Homepage.php
+++ Homepage.php
@@ -17,9 +17,11 @@
 <body>
     <?php if($_SESSION["ROLE"] == "admin"): ?>
         <h1>You are an admin</h1>
+        <a href="user-profile.php">Profile</a>
         <a href="Homepage.php?LOGOUT=1">Logout</a>
     <?php endif; ?>
     <?php if($_SESSION["ROLE"] == "lecturer"): ?>
+        <a href="user-profile.php">Profile</a>
         <a href="feedback.html">Feedback Form</a> <br><br>
         <a href="booking.html">Booking Application</a> <br> <br>
         <a href="availableRoom.php">Browse Available Rooms</a> <br>
@@ -27,6 +29,8 @@
     <?php endif; ?>
     <?php if($_SESSION["ROLE"] == "space_manager"): ?>
         <h1>You are a space manager</h1>
+        <a href="user-profile.php">Profile</a>
+        <a href="approvalscopy.php">Approvals</a>
         <a href="Homepage.php?LOGOUT=1">Logout</a>
     <?php endif; ?>
 </body>
